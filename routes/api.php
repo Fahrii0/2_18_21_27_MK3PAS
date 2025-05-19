e79d92e8e86592a2e831658433cdaa79a26b2e6a
Fahrii0 <fahrizzain07@gmail.com>
2025-05-19 15:08:29 +0700

    UserSwagger

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\PeminjamanController;
+use App\Http\Controllers\Api\UserController;
+
+
 
 Route::group([], function () {
     Route::get('category', [CategoryController::class, 'listCategory']);
 
-    
+    Route::apiResource('users', UserController::class);
     Route::apiResource('peminjaman', PeminjamanController::class);
 });
